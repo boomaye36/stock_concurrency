ec5dd79a21163fadf570646f4b770f8c7a16d300
boomaye36 <timxxz@naver.com>
2023-09-13 20:30:25 +0900

    kafka producer

--- src/main/java/com/example/stock_concurrency/service/ApplyService.java
+++ src/main/java/com/example/stock_concurrency/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.stock_concurrency.service;
 
 import com.example.stock_concurrency.domain.Coupon;
+import com.example.stock_concurrency.producer.CouponCreateProducer;
 import com.example.stock_concurrency.repository.CouponCountRepository;
 import com.example.stock_concurrency.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -9,18 +10,22 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+    private final CouponCreateProducer couponCreateProducer;
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
 
     public void apply(Long userId){
 
-        long count = couponCountRepository.increment();
-
+        Long count = couponCountRepository.increment();
+       // Long count = couponRepository.count();
         if (count > 100){
             return;
         }
-        couponRepository.save(new Coupon(userId));
+        //couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
     }
 }
+//docker exec -it kafka kafka-console-consumer.sh --topic coupon_create --bootstrap-server localhost:9092 --key-deserializer "org.apache.kafka.common.serialization.StringDeserializer" --value-deserializer "org.apache.kafka.common.serialization.LongDeserializer"
